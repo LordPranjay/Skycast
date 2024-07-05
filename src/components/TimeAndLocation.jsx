14aa3a988b8c02d8a5e265120bb204589abcbf13
Pranjay Yadav <ypranjay@gmail.com>
2024-07-05 10:34:07 +0530

    first commit

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TimeAndLocation = ({weather}) => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-xl font-extralight">{weather.formattedLocalTime}</p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-3xl font-medium">{`${weather.name}, ${weather.country}`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react"; /*imports react library*/
+import ReactDOM from "react-dom/client"; /*imports react-dom client library*/
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// This code sets up the basic structure of a React application, rendering the App component as the root of your application. The App component typically contains the rest of your application's components, managing their layout and behavior.
